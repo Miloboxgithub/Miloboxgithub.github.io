433911d37f34ed716e51eeace0378daa7a0c232f
Roxy <1012436741@qq.com>
2025-05-12 19:39:07 +0800

    Site updated: 2025-05-12 19:39:06

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/04/21/hello-world/"];function toRandomPost(){
+var posts=["2025/04/21/hello-world/","2025/05/12/前端实习/Javascript数据类型/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
