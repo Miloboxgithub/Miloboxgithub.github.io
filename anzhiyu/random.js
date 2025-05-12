26ab71f701faf6a5a41252269315ff4f422aec22
Roxy <1012436741@qq.com>
2025-05-12 22:51:02 +0800

    Site updated: 2025-05-12 22:51:01

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/04/21/hello-world/","2025/05/12/前端实习/Javascript数据类型/"];function toRandomPost(){
+var posts=["2025/04/21/hello-world/","2025/05/12/blog/hexo博客添加Live2d看板娘/","2025/05/12/前端实习/Javascript数据类型/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
